3b55d37a6f4103a15ef64a5ba1192e67f4e1694c
David Figueroa <dfigue92@gmail.com>
2018-05-07 18:00:49 -0700

    Deleted duplicate files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Home from './Home';
-import Hub from './Hub'
+import Hub from './pages/Hub'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <Home/>
         <Hub/>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hub from './pages/Hub'
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter basename='dfig'> 
+                    <Hub />
+                </BrowserRouter>
+                , document.getElementById('root'));
 registerServiceWorker();
