1b49e3976f93368fcd813fc5c227eb3e594e0563
David Figueroa <dfigue92@gmail.com>
2018-05-08 02:57:47 -0700

    Changed to hasrouter

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter> 
+ReactDOM.render(<HashRouter> 
                     <App/>
-                </BrowserRouter>
+                </HashRouter>
                 , document.getElementById('root'));
 registerServiceWorker();
